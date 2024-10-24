d57bb0137e4904066e1cf85c61f48151b5d0fd91
Piotr Branewski <pbranewski@student.agh.edu.pl>
2024-10-24 16:35:25 +0200

    lab 2 done

--- orm/src/main/java/pl/edu/agh/iisg/to/service/SchoolService.java
+++ orm/src/main/java/pl/edu/agh/iisg/to/service/SchoolService.java
@@ -4,12 +4,13 @@ import pl.edu.agh.iisg.to.dao.CourseDao;
 import pl.edu.agh.iisg.to.dao.GradeDao;
 import pl.edu.agh.iisg.to.dao.StudentDao;
 import pl.edu.agh.iisg.to.model.Course;
+import pl.edu.agh.iisg.to.model.Grade;
 import pl.edu.agh.iisg.to.model.Student;
+import pl.edu.agh.iisg.to.repository.StudentRepository;
 import pl.edu.agh.iisg.to.session.TransactionService;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
+import javax.persistence.EntityNotFoundException;
+import java.util.*;
 
 public class SchoolService {
 
@@ -21,17 +22,20 @@ public class SchoolService {
 
     private final GradeDao gradeDao;
 
-    public SchoolService(TransactionService transactionService, StudentDao studentDao, CourseDao courseDao, GradeDao gradeDao) {
+    private final StudentRepository studentRepository;
+
+    public SchoolService(TransactionService transactionService, StudentDao studentDao, CourseDao courseDao, GradeDao gradeDao, StudentRepository studentRepository) {
         this.transactionService = transactionService;
         this.studentDao = studentDao;
         this.courseDao = courseDao;
         this.gradeDao = gradeDao;
+        this.studentRepository = studentRepository;
     }
 
     public boolean enrollStudent(final Course course, final Student student) {
         // TODO - implement
         return transactionService.doAsTransaction( () -> {
-            if (course.studentSet().contains(student)){
+            if (studentRepository.findAllByCourseName(course.name()).contains(student)){
                 return false;
             }
             course.studentSet().add(student);
@@ -42,19 +46,46 @@ public class SchoolService {
 
     public boolean removeStudent(int indexNumber) {
         return transactionService.doAsTransaction(() ->{
+            Student student = studentRepository.getByIndexNumber(indexNumber).
+                    orElseThrow(() -> new EntityNotFoundException("student not found"));
 
+            studentRepository.remove(student);
+
+            return true;
         }).orElse(false);
     }
 
     public boolean gradeStudent(final Student student, final Course course, final float gradeValue) {
         // TODO - implement
-        return transactionService.doAsTransaction(() ->{
+        return transactionService.doAsTransaction(() -> {
+            Grade grade = new Grade(student, course, gradeValue);
 
+            gradeDao.save(grade);
+
+            student.gradeSet().add(grade);
+            course.gradeSet().add(grade);
+
+            return true;
         }).orElse(false);
     }
 
     public Map<String, List<Float>> getStudentGrades(String courseName) {
-        // TODO - implement
-        return Collections.emptyMap();
+        return transactionService.doAsTransaction(() -> {
+            Course course = courseDao.findByName(courseName)
+                    .orElseThrow(() -> new EntityNotFoundException("course not found"));
+            Map<String, List<Float>> report = new HashMap<>();
+
+            for (Grade grade : course.gradeSet()) {
+                String key = grade.student().firstName() + " " + grade.student().lastName();
+                report.computeIfAbsent(key, k -> new ArrayList<>()).add(grade.grade());
+            }
+
+            // Sort the grades for each student
+            for (Map.Entry<String, List<Float>> entry : report.entrySet()) {
+                Collections.sort(entry.getValue());
+            }
+
+            return report;
+        }).orElse(Collections.emptyMap());
     }
 }
